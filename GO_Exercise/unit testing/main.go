56221ed44c7f931953da30431ada72b38fd71f00
afzal442 <afzal442@gmail.com>
2021-09-25 14:17:03 +0530

    modifies the path and adds Unit testing
    Signed-off-by: afzal442 <afzal442@gmail.com>

---
+++ GO_Exercise/unit testing/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"math"
+)
+
+func Calc_power_of_x(x, y float64) float64 {
+
+	return math.Pow(x, y)
+}
+
+// func main() {
+// 	var x, y float64
+// 	fmt.Scanf("%f", &x)
+
+// 	y = 3
+// 	m := do_calc_power_of_x(x, y)
+// 	fmt.Println(m)
+// }
---
+++ GO_Exercise/unit testing/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+type test struct {
+	x        float64
+	y        float64
+	expected float64
+}
+
+func TestCal_power_of_x(t *testing.T) {
+	testcases := []test{
+		{2, 2, 4},
+		{2, 3, 8},
+		{2, 4, 16},
+	}
+
+	for _, tc := range testcases {
+		actual := Calc_power_of_x(tc.x, tc.y)
+		if actual != tc.expected {
+			// t.Errorf("do_cal_power_of_x(%f, %f) = %f; expected %f", tc.x, tc.y, actual, tc.expected)
+			t.Fail()
+		}
+	}
+}
