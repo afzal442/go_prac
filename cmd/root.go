1adbbd71cd22fbb8a6b2d0d4d57048ebeb65d19f
Afzal Ansari <afzal442@gmail.com>
2022-03-24 12:35:18 +0530

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -33,7 +33,7 @@ var rootCmd = &cobra.Command{
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
+Cobra is a CLI library for Go that empowers application.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
